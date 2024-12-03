4bf0a5ac04bb2309eac511b1638f7cd196307613
jongse7 <mmssjj72@naver.com>
2024-12-03 08:14:55 +0000

    feat: 리브 봇 추가 및 서버 유니크 id 저장 로직

--- src/server/component/card/server_card.tsx
+++ src/server/component/card/server_card.tsx
@@ -79,10 +79,9 @@ export default function ServerCard({
             }
             // 리브가 없는 서버 - 리브봇 추가 로직
             if (!isRiv) {
-              const id: string = guildId;
               const botAddUrl: string = `${
                 import.meta.env.VITE_BOT_ADD
-              }&guild_id=${guildId}&redirect_uri=https://riv-frontend.vercel.app/riv-redirect?id=${id}`;
+              }&guild_id=${guildId}`;
               handleTap({ url: botAddUrl });
             }
           }}
