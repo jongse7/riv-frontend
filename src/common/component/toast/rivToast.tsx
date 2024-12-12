f1f3e20501d92aa0f59ee3bc24c90ca34cd5e326
jongse7 <mmssjj72@naver.com>
2024-12-12 12:48:26 +0000

    style: toast css 설정

--- src/common/component/toast/rivToast.tsx
+++ src/common/component/toast/rivToast.tsx
@@ -1,6 +1,7 @@
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 
 export default function RivToast() {
-  return <ToastContainer />;
+  return <ToastContainer position="bottom-right" />;
 }
